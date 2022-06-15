cc46abbe63fbd2563f2ec8a512fe4c08a00ef12a
lauramarson <laurampinheiro17@gmail.com>
2022-06-14 22:50:04 -0300

    fix tab bar and change homevc

--- AnimalsApp/AnimalsApp/Views/HomeViewController/HomeViewController.swift
+++ AnimalsApp/AnimalsApp/Views/HomeViewController/HomeViewController.swift
@@ -8,22 +8,34 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.register(UINib(nibName: "AnimalTableViewCell", bundle: nil), forCellReuseIdentifier: "Animal")
+    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Animal") as? AnimalTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
     }
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension HomeViewController: UITableViewDelegate {
+    
 }
