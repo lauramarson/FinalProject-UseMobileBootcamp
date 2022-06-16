a5c5761a3507ac9b8bfbb3b6c38971c978a4ec14
lauramarson <laurampinheiro17@gmail.com>
2022-06-16 13:49:03 -0300

    UI changes

---
+++ AnimalsApp/AnimalsApp/DesignSystem/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  AnimalsApp
+//
+//  Created by Laura Pinheiro Marson on 16/06/22.
+//
+
+import UIKit
+
+extension UIColor {
+    static let blueTabBarColor: UIColor? = UIColor(named: "blueTabBarColor")
+    static let blueTextColor: UIColor? = UIColor(named: "blueTextColor")
+   
+}
--- AnimalsApp/AnimalsApp/Services/WebServices.swift
+++ AnimalsApp/AnimalsApp/Services/WebServices.swift
@@ -9,16 +9,17 @@ import Foundation
 import Alamofire
 
 protocol WebServicesContract: AnyObject {
+    var endpoint: String { get }
     func fetchAnimals(completion: @escaping (Result<[Animal], Error>) -> ())
     func registerAnimal()
 }
 
 class WebServices: WebServicesContract {
-    let urlString = "https://bootcamp-ios-api.herokuapp.com/api/v1/animals"
+    let endpoint: String =   "https://bootcamp-ios-api.herokuapp.com/api/v1/animals"
     
     func fetchAnimals(completion: @escaping (Result<[Animal], Error>) -> ()) {
         
-        AF.request(urlString)
+        AF.request(endpoint)
             .validate()
             .responseDecodable(of: Animals.self) { (response) in
                 switch response.result {
