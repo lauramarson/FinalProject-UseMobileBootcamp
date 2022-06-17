58003d16be559b7c673aa35faab37f0d46b040c4
lauramarson <laurampinheiro17@gmail.com>
2022-06-17 17:24:11 -0300

    add imageView border in tableViewCell

--- AnimalsApp/AnimalsApp/DesignSystem/Colors.swift
+++ AnimalsApp/AnimalsApp/DesignSystem/Colors.swift
@@ -10,5 +10,7 @@ import UIKit
 extension UIColor {
     static let blueTabBarColor: UIColor? = UIColor(named: "blueTabBarColor")
     static let blueTextColor: UIColor? = UIColor(named: "blueTextColor")
+    
+    static let lightGray: UIColor? = UIColor(named: "lightGray")
    
 }
