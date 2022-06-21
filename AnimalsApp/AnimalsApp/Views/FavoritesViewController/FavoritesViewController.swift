b6de8422f40a62d07188cdbe17a401e9b445fbdf
lauramarson <laurampinheiro17@gmail.com>
2022-06-20 22:23:39 -0300

    remove favorites screen

--- AnimalsApp/AnimalsApp/Views/FavoritesViewController/FavoritesViewController.swift
+++ AnimalsApp/AnimalsApp/Views/FavoritesViewController/FavoritesViewController.swift
@@ -9,93 +9,13 @@ import UIKit
 
 class FavoritesViewController: UIViewController {
     
-    var favoritesVM: FavoritesViewModel?
-    
-    @IBOutlet weak var tableView: UITableView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        favoritesVM = FavoritesViewModel()
         
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-        tableView.register(UINib(nibName: "AnimalTableViewCell", bundle: nil), forCellReuseIdentifier: "Animal")
-
-        setNavigationItems()
-        favoritesVM?.getFavoriteAnimals { [weak self] in            self?.tableView.reloadData()
         }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        favoritesVM?.getFavoriteAnimals { [weak self] in            self?.tableView.reloadData()
-        }
-    }
-
-    private func setNavigationItems() {
-        title = "Favoritos"
-        
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithOpaqueBackground()
-        appearance.titleTextAttributes = [
-            NSAttributedString.Key.foregroundColor: UIColor.blueTextColor ?? UIColor.blue,
-            NSAttributedString.Key.font: UIFont(name: "OpenSans", size: 20) ?? UIFont.systemFont(ofSize: 20)]
-        navigationController?.navigationBar.standardAppearance = appearance
-        navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
-        
-        navigationItem.backButtonTitle = ""
-    }
 
 }
 
-// MARK: TableView Data Source
-extension FavoritesViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return favoritesVM?.numberOfRows() ?? 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Animal", for: indexPath) as? AnimalTableViewCell else {
-            return UITableViewCell()
-        }
-        
-        cell.animal = favoritesVM?.modelAt(indexPath.row)
-        cell.index = indexPath.row
-        cell.delegate = self
-        cell.configure()
-        
-        return cell
-    }
 
-}
 
-// MARK: TableView Delegate
-extension FavoritesViewController: UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let detailVC = DetailViewController(nibName: "DetailViewController", bundle: nil)
-        
-        let animal = favoritesVM?.modelAt(indexPath.row)
-        detailVC.animal = animal
-
-        navigationController?.pushViewController(detailVC, animated: true)
-        
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-}
-
-// MARK: Action Delegate Protocol
-extension FavoritesViewController: ActionDelegateProtocol {
-    func addFavoriteTapped(at index: Int, with image: Data) {
-    }
-    
-    func removeFavoriteTapped(at index: Int) {
-        favoritesVM?.removeFavorite(at: index) { [weak self] in
-            self?.tableView.reloadData()
-        }
-    }
-}
 
